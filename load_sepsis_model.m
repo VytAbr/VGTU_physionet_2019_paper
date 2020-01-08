de2ba6d2af3ccdb989cd7ef54740b99ee51e2785
VytAbr <36620497+VytAbr@users.noreply.github.com>
2020-01-08 16:20:54 +0200

    Update load_sepsis_model.m

--- load_sepsis_model.m
+++ load_sepsis_model.m
@@ -6,7 +6,7 @@ model_long = 'model_l';
 model_standartize = 'standartize_values_ab';
 
 model{1,1} = load(model_short);
-model{1,2} = loadmodel_medium);
+model{1,2} = load(model_medium);
 model{1,3} = load(model_long);
 model{1,4} = load(model_standartize);
 
